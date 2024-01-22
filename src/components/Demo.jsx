fc3e80f634a8631defed8a1b5094c1d2daa26364
Nwakodo Ifeanyi <46559372+ify12345@users.noreply.github.com>
2024-01-22 21:40:45 +0100

    comm4

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,46 @@
-
+import { useState,useEffect } from "react"
+import {copy, linkIcon,loader, tick } from "../assets"
 
 const Demo = () => {
+
+   const[article, setArticle] = useState({
+    url: "",
+    summary: ""
+   })
+
+ const handleSubmit = async (e) => {
+      alert("submitted");
+ }
   return (
-    <div>
-      Demo
+   <section className="mt-16 w-full max-w-xl">
+
+    {/* search */}
+
+
+    <div className="flex flex-col w-full gap-2">
+
+
+      <form action="" className="relative flex justify-center items-center" onSubmit={()=>{handleSubmit}}>
+          <img src={linkIcon} alt="" className="absolute left-0 my-2 ml-3 w-5 " />
+          <input value={article.url} type="url" name="" id="" placeholder="Enter a URL"
+           onChange={(e)=>setArticle({
+            ...article, url:e.target.value
+          })} 
+            required
+            className="url_input peer"
+          />
+          <button type="submit" className="submit_btn ">
+           &#x2936;
+          </button>
+      </form>
+
+
+
+      {/* bROWSE URL HISTORY */}
+
     </div>
+    {/* search results */}
+   </section>
   )
 }
 
